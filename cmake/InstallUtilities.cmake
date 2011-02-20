3e055059f3cd99fbb355b66fada63eb605736777
Lionel Heng <hengli@student.ethz.ch>
2011-02-21 00:06:34 +0100

    Added message for RGBD cameras.

--- cmake/InstallUtilities.cmake
+++ cmake/InstallUtilities.cmake
@@ -54,6 +54,7 @@ FUNCTION(PIXHAWK_INSTALL)
 	${CMAKE_SOURCE_DIR}/src/comm/lcm/gl_overlay_message_t.h
 	${CMAKE_SOURCE_DIR}/src/comm/lcm/obstacle_map_message_t.h
 	${CMAKE_SOURCE_DIR}/src/comm/lcm/point_cloud_message_t.h
+	${CMAKE_SOURCE_DIR}/src/comm/lcm/rgbd_camera_image_message_t.h
 	${CMAKE_SOURCE_DIR}/src/comm/lcm/virtual_scan_message_t.h
   )
   INSTALL(FILES ${MAVCONNLCM_HEADERS} DESTINATION ${PIXHAWK_INCLUDE_INSTALL_PATH}/comm/lcm/)
