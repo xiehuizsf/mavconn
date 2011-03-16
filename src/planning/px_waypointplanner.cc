d968f17d9cd5e96c027af8648a23b53e8f911393
pixhawk <pixhawk@pixhawk01.(none)>
2011-03-16 17:59:51 +0100

    fixed out of bounds error

--- src/planning/px_waypointplanner.cc
+++ src/planning/px_waypointplanner.cc
@@ -206,23 +206,16 @@ void send_waypoint(uint8_t target_systemid, uint8_t target_compid, uint16_t seq)
 
 void send_waypoint_request(uint8_t target_systemid, uint8_t target_compid, uint16_t seq)
 {
-    if (seq < waypoints->size())
-    {
-        mavlink_message_t msg;
-        mavlink_waypoint_request_t wpr;
-        wpr.target_system = target_systemid;
-        wpr.target_component = target_compid;
-        wpr.seq = seq;
-        mavlink_msg_waypoint_request_encode(systemid, compid, &msg, &wpr);
-        mavlink_message_t_publish(lcm, "MAVLINK", &msg);
-        if (verbose) printf("Sent waypoint request %u to ID %u\n", wpr.seq, wpr.target_system);
+	mavlink_message_t msg;
+	mavlink_waypoint_request_t wpr;
+	wpr.target_system = target_systemid;
+	wpr.target_component = target_compid;
+	wpr.seq = seq;
+	mavlink_msg_waypoint_request_encode(systemid, compid, &msg, &wpr);
+	mavlink_message_t_publish(lcm, "MAVLINK", &msg);
+	if (verbose) printf("Sent waypoint request %u to ID %u\n", wpr.seq, wpr.target_system);
 
-        usleep(paramClient->getParamValue("PROTOCOLDELAY"));
-    }
-    else
-    {
-        if (verbose) printf("ERROR: index out of bounds\n");
-    }
+	usleep(paramClient->getParamValue("PROTOCOLDELAY"));
 }
 
 /*
