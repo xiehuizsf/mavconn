eed07211a169494721b42f76d9b48fe2eca8a658
Lionel Heng <hengli@student.ethz.ch>
2011-11-29 10:07:37 +0100

    Added camera to world rotation matrix to virtual scan message.

--- src/comm/lcm/virtual_scan_message_t.h
+++ src/comm/lcm/virtual_scan_message_t.h
@@ -28,6 +28,7 @@ struct _virtual_scan_message_t
     int16_t    range_min;
     int16_t    range_max;
     double     origin[3];
+    double     camera_to_world_rot[9];
     int32_t    num_rays;
     int16_t    *ray_endpoints;
 };
@@ -42,6 +43,9 @@ typedef void(*virtual_scan_message_t_handler_t)(const lcm_recv_buf_t *rbuf,
 int virtual_scan_message_t_publish(lcm_t *lcm, const char *channel, const virtual_scan_message_t *p);
 virtual_scan_message_t_subscription_t* virtual_scan_message_t_subscribe(lcm_t *lcm, const char *channel, virtual_scan_message_t_handler_t f, void *userdata);
 int virtual_scan_message_t_unsubscribe(lcm_t *lcm, virtual_scan_message_t_subscription_t* hid);
+int virtual_scan_message_t_subscription_set_queue_capacity(virtual_scan_message_t_subscription_t* subs, 
+                              int num_messages);
+
 
 int  virtual_scan_message_t_encode(void *buf, int offset, int maxlen, const virtual_scan_message_t *p);
 int  virtual_scan_message_t_decode(const void *buf, int offset, int maxlen, virtual_scan_message_t *p);
