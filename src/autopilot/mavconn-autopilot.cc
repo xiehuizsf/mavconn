9e7259dfab9b627e3684e7e76baa50616b250ed1
Hui Xie <xie1@ualberta.ca>
2013-04-28 16:38:47 -0600

    add boost now, test MainApp.cc first

--- src/autopilot/mavconn-autopilot.cc
+++ src/autopilot/mavconn-autopilot.cc
@@ -1,7 +1,35 @@
-#include<iostream>
+/**
+   @author Hui Xie <xie1@ualberta.ca>
+   \author Bryan Godbolt <godbolt@ece.ualberta.ca>
+   @author Nikos Vitzilaios <nvitzilaios@ualberta.ca>
+   @author Aakash Vasudevan <avasudev@ualberta.ca>
+   \author Hasitha Senanayake <senanaya@ualberta.ca>
 
+//   \mainpage
+//   This project contains the code for the autopilot system.  This documentation is available on the
+//   local network at http://doc/autopilot/html/index.html .
+//   The central version should be used (as opposed to a locally generated version) since it is regenerated
+//   every time a push is made to the central repository.
+
+//   \p The contents of the root folder are
+//   - \b doc/ project documentation
+//   - \b README describes folder structure
+//   - \b src/ source code
+//   - \b utils/ sample programs to demonstrate various aspects of the system (e.g., counter board i/o)
+//   - \b extern/ external libraries used to build the autopilot
+
+//   @note all debugging messages should be printed using the ::debug() function.  See the Debug class.
+ */
+#include<iostream>
+/* Boost Headers */
+#include <boost/thread.hpp>
+
+/* Project Headers */
+#include "MainApp.h"
 using namespace std;
-main()
+int main()
 {
-	cout<<"hello"<<endl;
+	cout<<"Welcome to Mavconn-autopilot"<<endl;
+	MainApp m;
+//	pid_channel roll;
 }
